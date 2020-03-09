650c61c6263fb7ec07e056565e35ec685c1e980d
marissaverdonck <marissa.verdonck@hva.nl>
2020-03-09 16:20:46 +0100

    weatherApi

--- server.js
+++ server.js
@@ -2,21 +2,28 @@ const express = require('express')
 const app = express()
 const port = 3000
 const fetch = require('node-fetch');
+const obaData = require('./modules/obaapi')
+const weatherData = require('./modules/weatherApi')
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+
+
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-    //   fetch('')
+  weatherData.getWeather()
+    .then(function(data) {
+      console.log(data)
+    })
+  res.render('index')
+
 })
 
-app.get('/overview', (req, res) => {
-
-  //   fetch('')
-  res.render('overview')
-
+app.get('/results', (req, res) => {
+  res.render('results')
 })
 
 
