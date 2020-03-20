e9bbbae8cdfe0c7a32e325466a382233a3970f66
marissaverdonck <marissa.verdonck@hva.nl>
2020-03-20 16:27:22 +0100

    build

--- scripts/build-js.js
+++ scripts/build-js.js
@@ -2,7 +2,7 @@ const gulp = require('gulp')
 const concat = require('gulp-concat');
 
 return gulp.src([
-    './public/js/*.js',
+    './public/js/script.js',
   ])
   .pipe(concat('script.js'))
   .pipe(gulp.dest('./static/'))
